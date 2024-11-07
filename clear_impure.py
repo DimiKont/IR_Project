f842feca7dd2c7ef5cb1eb24304164d0935f2b67
Jim <dimkon2003@gmail.com>
2024-11-07 15:50:30 +0200

    Added a script that cleans initial data from weird tokens
    There were some tokens like %C5% and %B3% that needed cleaning

---
+++ clear_impure.py
@@ -0,0 +1,14 @@
+import csv
+import re
+
+input_file = 'people_wiki.csv'
+output_file = 'pure_people_wiki.csv'
+pattern = re.compile(r'%[0-9A-Fa-f]{2}%')
+
+with open(input_file, mode='r', newline='') as infile, open(output_file, mode='w', newline='') as outfile:
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile)
+    
+    for row in reader:
+        if not any(pattern.search(cell) for cell in row):
+            writer.writerow(row)
